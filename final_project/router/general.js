612e1038787ee22fcc05e6dcce50e07e108452af
Sarah  Saad <sarahsaad@Sarahs-MacBook-Pro.local>
2025-03-11 10:34:11 +0000

    tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -68,5 +69,59 @@ public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
+
+async function getBooks(){
+  try{
+    const res = await axios.get("http://localhost:6000");
+      console.log(res.data);
+  }
+  catch(e) {
+    return 'Error fetching books'
+  }
+}
+
+// Call the async function to execute the request
+// getBooks();
+
+
+async function getBookByIsbn(){
+  try{
+    const res = await axios.get("http://localhost:6000/isbn/2");
+      console.log(res.data);
+  }
+  catch(e) {
+    return 'Error fetching books'
+  }
+}
+
+// Call the async function to execute the request
+// getBookByIsbn();
+
+
+async function getBookByAuthor(){
+  try{
+    const res = await axios.get("http://localhost:6000/author/sarah009");
+      console.log(res.data);
+  }
+  catch(e) {
+    return 'Error fetching books'
+  }
+}
+
+// Call the async function to execute the request
+ //getBookByAuthor();
+
+async function getBookByTitle(){
+  try{
+    const res = await axios.get("http://localhost:6000/title/The Book Of Job");
+      console.log(res.data);
+  }
+  catch(e) {
+    return 'Error fetching books'
+  }
+}
+
+// Call the async function to execute the request
+getBookByTitle();
 
 module.exports.general = public_users;
